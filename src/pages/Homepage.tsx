e98d7c9e0ae1f662c2a39d622621f361cbf924e9
cchampou <clement@champouillon.com>
2022-04-19 23:47:36 +0200

    feat: add links to the homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,10 +1,27 @@
 import { Helmet } from 'react-helmet';
+import { Card } from 'baseui/card';
+import { NavLink } from 'react-router-dom';
+import { Cell, Grid } from 'baseui/layout-grid';
 
 function Homepage() {
   return (
-    <Helmet>
-      <title>Homepage</title>
-    </Helmet>
+    <>
+      <Helmet>
+        <title>Homepage</title>
+      </Helmet>
+      <Grid>
+        <Cell span={12}>
+          <Card>
+            <h1>Video maker?</h1>
+            <NavLink to="/video">
+              Check out how to convert a video or audio quickly and easily
+            </NavLink>
+            <h1>You&apos;re into crypto?</h1>
+            <NavLink to="/crypto">Check the most common crypto prices</NavLink>
+          </Card>
+        </Cell>
+      </Grid>
+    </>
   );
 }
 
